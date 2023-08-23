602cbaa4e32b07eb7a6c018c46ccbbc171208e33
unknown <sachin3145@outlook.com>
2023-08-24 00:14:42 +0530

    Integrate with Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
 const page = () => {
   return (
-    <div>Hello</div>
-  );
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a CSE Major and Aspiring Software Engineer!
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-titel">
+            Sachin Yadav
+          </Heading>
+          <p>Competitive Programmer / Developer / Thinker</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default page;
