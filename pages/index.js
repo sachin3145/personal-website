4e000f9348c03e784de032eba6d507e01ce541a3
unknown <sachin3145@outlook.com>
2023-08-28 02:55:13 +0530

    Populated HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import {Container, Box, Heading, Image, useColorModeValue, Button, Spacer, Flex } from "@chakra-ui/react";
 import Section from "../components/section";
+import Paragraph from "../components/paragraph";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import NextLink from "next/link";
+import { JourneySection, JourneyYear } from "../components/journey";
 
 const page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a CSE Major and Aspiring Software Engineer!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -27,18 +37,59 @@ const page = () => {
             maxWidth="100px"
             display="inline-block"
             borderRadius="full"
-            src={"/images/sachin.jpeg"}
+            src={'/images/sachin.jpeg'}
             alt="Profile Image"
           />
         </Box>
       </Box>
       <Section delay={0.1}>
         <Heading as="h3" variant="section-title">
+          About
+        </Heading>
+        <Paragraph>
+          Sachin is a dedicated student currently pursuing a Bachelor's Degree
+          in Computer Science and Engineering at the University School of
+          Information, Communication, and Technology, GGSIPU.
+        </Paragraph>
+        <Box my={4}>
+          <NextLink href="/about">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              MORE ABOUT ME
+            </Button>
+          </NextLink>
+        </Box>
+        <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>
-          Paragraph
-        </p>
+        <Paragraph>
+          Their passion lies in honing their problem-solving skills and adeptly
+          applying them to develop innovative solutions aimed at enhancing
+          workplace efficiency and tackling novel problems.
+        </Paragraph>
+        <Box my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              SEE MY WORK
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          My Journey
+        </Heading>
+        <JourneySection>
+          <JourneyYear>2003</JourneyYear>
+          Born in New Delhi, India
+        </JourneySection>
+        <JourneySection>
+          <JourneyYear>2021</JourneyYear>
+          Completed Senior School (CBSE)
+        </JourneySection>
+        <JourneySection>
+          <JourneyYear>2021 - Present</JourneyYear>
+          B.Tech. in Computer Science & Engineering
+        </JourneySection>
       </Section>
     </Container>
   )
