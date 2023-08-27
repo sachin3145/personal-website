d1da574a3f730d7921dc2a2a1561b2d05f9312c8
unknown <sachin3145@outlook.com>
2023-08-27 19:07:36 +0530

    Added Profile Picture, minor UIupdates

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
-
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 const page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
         Hello, I&apos;m a CSE Major and Aspiring Software Engineer!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -12,6 +11,23 @@ const page = () => {
             Sachin Yadav
           </Heading>
           <p>Engineer / Competitive Programmer / Developer</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src={"/images/sachin.jpeg"}
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
