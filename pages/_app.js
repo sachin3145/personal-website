df9407966d72669644299642fd4532b3c51e9f7c
unknown <sachin3145@outlook.com>
2023-08-30 00:18:08 +0530

    Implement Page Transition using framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layouts/main"
 import Fonts from "../components/fonts";
 import theme from "../libs/theme";
+import { AnimatePresence } from "framer-motion";
 
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
       <Layout router={router}>
-        <Component {...pageProps} key={router.route} />
+        <AnimatePresence mode="wait" initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </Layout>
     </ChakraProvider>
-  );
+  )
 }
 
 export default Website;
--- pages/works.js
+++ pages/works.js
@@ -6,7 +6,7 @@ import thumbAuthenticator from '../public/images/works/authenticator/thumbnail.p
 import thumbVisualizer from '../public/images/works/sorting-visualizer/thumbnail.png'
 import thumbParticle from '../public/images/works/particlejs/thumbnail.png'
 import { useEffect, useState } from 'react'
-
+import Layout from "../components/layouts/article";
 
 
 const Works = () => {
@@ -20,38 +20,52 @@ const Works = () => {
   return (
     <>
       {domLoaded && (
-        <Container>
-          <Heading as="h3" fontSize={20} mb={4}>
-            Works
-          </Heading>
-          <SimpleGrid columns={[1, 1, 2]} gap={6}>
-            <Section>
-              <WorkGridItem
-                id="authenticator"
-                title="Next-Authenticator"
-                thumbnail={thumbAuthenticator}
-              >
-                Demonstrating user authentication
-              </WorkGridItem>
-            </Section>
-            <Section>
-              <WorkGridItem id="inloop" title="InLoop" thumbnail={thumbInloop}>
-                A real-time code editor
-              </WorkGridItem>
-            </Section>
-            <Section>
-              <WorkGridItem id="sorting-visualizer" title="Sorting Visualizer" thumbnail={thumbVisualizer}>
-                Visual demonstration of sorting algorithms
-              </WorkGridItem>
-            </Section>
-            <Section>
-              <WorkGridItem id="particlejs" title="Particle Js" thumbnail={thumbParticle}>
-                Canvas Implementation of particle interaction
-              </WorkGridItem>
-            </Section>
-          </SimpleGrid>
-          <GridItemStyle />
-        </Container>
+        <Layout>
+          <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+              Works
+            </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+              <Section>
+                <WorkGridItem
+                  id="authenticator"
+                  title="Next-Authenticator"
+                  thumbnail={thumbAuthenticator}
+                >
+                  Demonstrating user authentication
+                </WorkGridItem>
+              </Section>
+              <Section>
+                <WorkGridItem
+                  id="inloop"
+                  title="InLoop"
+                  thumbnail={thumbInloop}
+                >
+                  A real-time code editor
+                </WorkGridItem>
+              </Section>
+              <Section>
+                <WorkGridItem
+                  id="sorting-visualizer"
+                  title="Sorting Visualizer"
+                  thumbnail={thumbVisualizer}
+                >
+                  Visual demonstration of sorting algorithms
+                </WorkGridItem>
+              </Section>
+              <Section>
+                <WorkGridItem
+                  id="particlejs"
+                  title="Particle Js"
+                  thumbnail={thumbParticle}
+                >
+                  Canvas Implementation of particle interaction
+                </WorkGridItem>
+              </Section>
+            </SimpleGrid>
+            <GridItemStyle />
+          </Container>
+        </Layout>
       )}
     </>
   )
