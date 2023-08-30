96cad4d6c51711597420b4b47fcdbd31fd2a6cc2
unknown <sachin3145@outlook.com>
2023-08-30 17:15:49 +0530

    Fixed Link Colors in NAVBAR (LIGHT MODE)

--- components/navbar.js
+++ components/navbar.js
@@ -20,7 +20,7 @@ import ThemeToggleButton from "./theme-toggle-button";
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href; 
-  const inactiveColor = useColorModeValue('gray.200', 'whiteAlpha.900');
+  const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900');
   return (
     <NextLink href={href}>
       <Link
