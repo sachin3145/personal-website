6f3d27d889cf7c7f377964e92e38afd5c25326c5
unknown <sachin3145@outlook.com>
2023-09-02 00:32:34 +0530

    Added Coding Profiles

--- pages/works.js
+++ pages/works.js
@@ -1,10 +1,14 @@
 import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
 import Section from "../components/section";
-import { GridItemStyle, WorkGridItem } from "../components/grid-item";
+import { GridItemStyle, WorkGridItem, GridItem } from "../components/grid-item";
 import thumbInloop from "../public/images/works/inloop/thumbnail.png";
 import thumbAuthenticator from '../public/images/works/authenticator/thumbnail.png'
 import thumbVisualizer from '../public/images/works/sorting-visualizer/thumbnail.png'
 import thumbParticle from '../public/images/works/particlejs/thumbnail.png'
+import thumbLeetcode from '../public/images/profiles/leetcode.png'
+import thumbCodeforces from '../public/images/profiles/codeforces.png'
+import thumbCodechef from '../public/images/profiles/codechef.png'
+import thumbHackerRank from '../public/images/profiles/hackerrank.png'
 import { useEffect, useState } from 'react'
 import Layout from "../components/layouts/article";
 
@@ -22,6 +26,41 @@ const Works = () => {
       {domLoaded && (
         <Layout>
           <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+              Coding Profiles
+            </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+              <Section>
+                <GridItem
+                  href="https://leetcode.com/sachin3145/"
+                  thumbnail={thumbLeetcode}
+                  title="LeetCode"
+                />
+              </Section>
+              <Section>
+                <GridItem
+                  href="https://codechef.com/users/sakumo"
+                  thumbnail={thumbCodechef}
+                  title="CodeChef"
+                />
+              </Section>
+              <Section>
+                <GridItem
+                  href="https://codeforces.com/profile/sachin3145"
+                  thumbnail={thumbCodeforces}
+                  title="CodeForces"
+                />
+              </Section>
+              <Section>
+                <GridItem
+                  href="https://www.hackerrank.com/sachin3145"
+                  thumbnail={thumbHackerRank}
+                  title="HackerRank"
+                />
+              </Section>
+            </SimpleGrid>
+            <GridItemStyle />
+            <Divider mb={4} />
             <Heading as="h3" fontSize={20} mb={4}>
               Works
             </Heading>
